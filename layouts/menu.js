eff4b0d05a1ca4dd96861dcb44c3a92cb4b98ada
Muhibbudin Suretno <muhibbudinsuretno1@gmail.com>
2018-02-10 10:27:43 +0700

    Add more menu

--- layouts/menu.js
+++ layouts/menu.js
@@ -2,7 +2,7 @@ export default {
   items: [
     {
       name: 'Dashboard',
-      url: '/',
+      url: '/dashboard',
       icon: 'icon-speedometer',
       badge: {
         variant: 'primary',
@@ -11,47 +11,133 @@ export default {
     },
     {
       title: true,
-      name: 'UI elements'
+      name: 'Theme',
+      class: '',
+      wrapper: {
+        element: '',
+        attributes: {}
+      }
     },
     {
+      name: 'Colors',
+      url: '/theme/colors',
+      icon: 'icon-drop'
+    },
+    {
+      name: 'Typography',
+      url: '/theme/typography',
+      icon: 'icon-pencil'
+    },
+    {
+      title: true,
       name: 'Components',
-      url: '/components',
+      class: '',
+      wrapper: {
+        element: '',
+        attributes: {}
+      }
+    },
+    {
+      name: 'Base',
+      url: '/base',
       icon: 'icon-puzzle',
       children: [
         {
-          name: 'Buttons',
-          url: '/components/buttons',
-          icon: 'icon-puzzle'
-        },
-        {
-          name: 'Social Buttons',
-          url: '/components/social-buttons',
+          name: 'Breadcrumbs',
+          url: '/base/breadcrumbs',
           icon: 'icon-puzzle'
         },
         {
           name: 'Cards',
-          url: '/components/cards',
+          url: '/base/cards',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Carousels',
+          url: '/base/carousels',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Collapses',
+          url: '/base/collapses',
           icon: 'icon-puzzle'
         },
         {
           name: 'Forms',
-          url: '/components/forms',
+          url: '/base/forms',
           icon: 'icon-puzzle'
         },
         {
-          name: 'Modals',
-          url: '/components/modals',
+          name: 'Jumbotrons',
+          url: '/base/jumbotrons',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'List Groups',
+          url: '/base/list-groups',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Navs',
+          url: '/base/navs',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Paginations',
+          url: '/base/paginations',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Popovers',
+          url: '/base/popovers',
+          icon: 'icon-puzzle'
+        },
+        {
+          name: 'Progress Bars',
+          url: '/base/progress-bars',
           icon: 'icon-puzzle'
         },
         {
           name: 'Switches',
-          url: '/components/switches',
+          url: '/base/switches',
           icon: 'icon-puzzle'
         },
         {
           name: 'Tables',
-          url: '/components/tables',
+          url: '/base/tables',
           icon: 'icon-puzzle'
+        },
+        {
+          name: 'Tooltips',
+          url: '/base/tooltips',
+          icon: 'icon-puzzle'
+        }
+      ]
+    },
+    {
+      name: 'Buttons',
+      url: '/buttons',
+      icon: 'icon-cursor',
+      children: [
+        {
+          name: 'Standard Buttons',
+          url: '/buttons/standard-buttons',
+          icon: 'icon-cursor'
+        },
+        {
+          name: 'Button Groups',
+          url: '/buttons/button-groups',
+          icon: 'icon-cursor'
+        },
+        {
+          name: 'Dropdowns',
+          url: '/buttons/dropdowns',
+          icon: 'icon-cursor'
+        },
+        {
+          name: 'Social Buttons',
+          url: '/buttons/social-buttons',
+          icon: 'icon-cursor'
         }
       ]
     },
@@ -61,9 +147,22 @@ export default {
       icon: 'icon-star',
       children: [
         {
+          name: 'Flags',
+          url: '/icons/flags',
+          icon: 'icon-star',
+          badge: {
+            variant: 'success',
+            text: 'NEW'
+          }
+        },
+        {
           name: 'Font Awesome',
           url: '/icons/font-awesome',
-          icon: 'icon-star'
+          icon: 'icon-star',
+          badge: {
+            variant: 'secondary',
+            text: '4.7'
+          }
         },
         {
           name: 'Simple Line Icons',
@@ -73,18 +172,40 @@ export default {
       ]
     },
     {
+      name: 'Charts',
+      url: '/charts',
+      icon: 'icon-pie-chart'
+    },
+    {
+      name: 'Notifications',
+      url: '/notifications',
+      icon: 'icon-bell',
+      children: [
+        {
+          name: 'Alerts',
+          url: '/notifications/alerts',
+          icon: 'icon-bell'
+        },
+        {
+          name: 'Badges',
+          url: '/notifications/badges',
+          icon: 'icon-bell'
+        },
+        {
+          name: 'Modals',
+          url: '/notifications/modals',
+          icon: 'icon-bell'
+        }
+      ]
+    },
+    {
       name: 'Widgets',
       url: '/widgets',
       icon: 'icon-calculator',
       badge: {
-        variant: 'danger',
+        variant: 'primary',
         text: 'NEW'
       }
-    },
-    {
-      name: 'Charts',
-      url: '/charts',
-      icon: 'icon-pie-chart'
     },
     {
       divider: true
@@ -119,6 +240,19 @@ export default {
           icon: 'icon-star'
         }
       ]
+    },
+    {
+      name: 'Download CoreUI',
+      url: 'http://coreui.io/vue/',
+      icon: 'icon-cloud-download',
+      class: 'mt-auto',
+      variant: 'success'
+    },
+    {
+      name: 'Try CoreUI PRO',
+      url: 'http://coreui.io/pro/vue/',
+      icon: 'icon-layers',
+      variant: 'danger'
     }
   ]
 }
